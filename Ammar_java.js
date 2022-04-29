f10cb4433ca3ad9882ca6f76ee1c1c614d19e044
Moviano <104652535+Moviano@users.noreply.github.com>
2022-04-29 22:14:30 +0200

    Ammar initial version

---
+++ Ammar_java.js
@@ -0,0 +1,82 @@
+function gotomyaccount(){
+    window.location.href = "html_assignment.html";
+}
+function gotonewspage(){
+    window.location.href="Untitled-1.html";
+}
+function switch_body() {
+    var element1 = document.body;
+    var element2=document.getElementById("navigationBar");
+    var element3=document.getElementById("navMenu");
+    var element4=document.getElementById("navSwitch");
+    var element5=document.getElementById("navNews");
+    var element6= document.getElementById("navLogo");
+    var element7= document.getElementById("sidebar");
+    var element8= document.getElementById("navHome");
+    var element9= document.getElementById("navShop");
+    var element10=document.getElementById("footer");
+    var element12=document.getElementById("switch");
+    var element11=document.getElementById("news");
+    var element13=document.getElementById("home");
+    var element14=document.getElementById("menu");
+    var element15=document.getElementById("logo");
+    var element16=document.getElementById("shop");
+    var element17=document.getElementById("sidebar");
+    var element18=document.getElementById("sidebarElement1");
+    var element19=document.getElementById("sidebarElement2");
+    var element20=document.getElementById("sidebarElement3");
+    var element21=document.getElementById("sidebarElement4");
+    var element22=document.getElementById("sidebarElement5");
+    var element23=document.getElementById("gallery1");
+    var element24=document.getElementById("desc1");
+    var element25=document.getElementById("gallery2");
+    var element26=document.getElementById("desc2");
+    var element27=document.getElementById("gallery3");
+    var element28=document.getElementById("desc3");
+    var element29=document.getElementById("gallery4");
+    var element30=document.getElementById("desc4");
+    var element31=document.getElementById("gallery5");
+    var element32=document.getElementById("desc5");
+    var element33=document.getElementById("gallery6");
+    var element34=document.getElementById("desc6");
+    /*var element25=document.getElementById("");
+    var element26=document.getElementById("");
+    var element27=document.getElementById("");
+    var element28=document.getElementById("");*/
+        element1.classList.toggle("dark-mode");
+        element2.classList.toggle("dark-mode");
+        element3.classList.toggle("dark-mode");
+        element4.classList.toggle("dark-mode");
+        element5.classList.toggle("dark-mode");
+        element6.classList.toggle("dark-mode");
+        element7.classList.toggle("dark-mode");
+        element8.classList.toggle("dark-mode");
+        element9.classList.toggle("dark-mode");
+        element10.classList.toggle("dark-mode");
+        element11.classList.toggle("dark-mode");
+        element12.classList.toggle("dark-mode");
+        element13.classList.toggle("dark-mode");
+        element14.classList.toggle("dark-mode");
+        element15.classList.toggle("dark-mode");
+        element16.classList.toggle("dark-mode");
+        element17.classList.toggle("dark-mode");
+        element18.classList.toggle("dark-mode");
+        element19.classList.toggle("dark-mode");
+        element20.classList.toggle("dark-mode");
+        element21.classList.toggle("dark-mode");
+        element22.classList.toggle("dark-mode");
+        element23.classList.toggle("dark-mode");
+        element24.classList.toggle("dark-mode");
+        element25.classList.toggle("dark-mode");
+        element26.classList.toggle("dark-mode");
+        element27.classList.toggle("dark-mode");
+        element28.classList.toggle("dark-mode");
+        element29.classList.toggle("dark-mode");
+        element30.classList.toggle("dark-mode");
+        element31.classList.toggle("dark-mode");
+        element32.classList.toggle("dark-mode");
+        element33.classList.toggle("dark-mode");
+        element34.classList.toggle("dark-mode");
+        /*element25.classList.toggle("dark-mode");
+        element26.classList.toggle("dark-mode");*/
+    }
---
+++ assignment.js
@@ -0,0 +1,34 @@
+//assignment1
+let NumberOne="10",NumberTwo="20";
+let concat=NumberOne+NumberTwo;
+console.log(concat.repeat(4)); 
+console.log(NumberTwo+"\n"+NumberOne);
+console.log(`${NumberTwo}
+${NumberOne}`);
+//assignment3
+console.log(`I'm In
+\\
+Love \\ """ '''
+++ With ++
+\"""\"""
+""JavaScript""`);
+console.log("I'm In\n\\\nLove \\ \"\"\" '''\n++ With ++\n\"\"\"\"\n\"\"JavaScript\"\"");
+//assignment4
+let a = "21";
+let b = "20";
+let con=b+a+"_";
+console.log(`_${a}_${con.repeat(3)}${b}_`); // _21_2021_2021_2021_20_
+///////////////////////////////////dfd
+let num = "10";
+
+// Solution One
+console.log(+num + +num); // 20
+
+// Solution Two
+console.log((++num) + (num-=2)); // 20
+
+// Solution Three
+console.log("Write Your Code Here"); // 20
+
+// Solution Four
+console.log("Write Your Code Here"); // 20
